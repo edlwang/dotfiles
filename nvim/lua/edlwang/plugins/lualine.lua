5b0b15720ee6ce24b795484ef427cc91a733eea0
Edward Wang <edward.wang@kitware.com>
2026-06-18 11:53:52 -0400

    docs(nvim): Add comment clarifying that using dracula for lualine is an intentional choice

--- nvim/lua/edlwang/plugins/lualine.lua
+++ nvim/lua/edlwang/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "dracula",
+				theme = "dracula", -- intentionally not tokyonight, i think dracula line looks better
 			},
 		})
 	end,
